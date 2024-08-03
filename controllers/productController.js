f9762b08b44cca5b9dfa58b24b87bac2d7c44e04
Martin Zalazar <martinzetav@gmail.com>
2024-08-03 16:04:04 -0300

    Se agrego la hoja para detalle de producto con sus respectiva ruta y controlador

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,9 +5,15 @@ const productController = {
     getByCategory(req, res){
         this.products = dataSource.load();
         const {category} = req.params;
-        const productsByCategory = this.products.filter(prod => prod.category == category);
+        this.products = this.products.filter(prod => prod.category == category);
         
-        res.render("productDetails", { productsByCategory, category });
+        res.render("index", { products, category });
+    },
+    getById(req, res){
+        this.products = dataSource.load();
+        const {id} = req.params;
+        const product = this.products.find(prod => prod.id == id);
+        res.render("productDetails", {product});
     }
 }
 
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/details/:category", productController.getByCategory);
+router.get("/:category", productController.getByCategory);
+router.get("/details/:id", productController.getById);
 
 module.exports = router;
