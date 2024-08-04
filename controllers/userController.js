47195433b290210ecb26ed325e8585dd01717305
Martin Zalazar <martinzetav@gmail.com>
2024-08-04 20:37:03 -0300

    se continua trabajando con express-validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require("node:path");
 const dataSource = require("../services/dataSource");
+const { validationResult } = require("express-validator");
 const usersFilePath = path.join(__dirname, '../data/users.json')
 
 const userController = {
@@ -8,19 +9,27 @@ const userController = {
         res.render("register");
     },
     createUser(req, res){
-        this.users = dataSource.load(usersFilePath);
-        const {firstName, lastName, phone, email, password} = req.body;
-        const newUser = {
-            id: this.users.length ? this.users[this.users.length - 1].id + 1: 1,
-            firstName,
-            lastName,
-            phone,
-            email,
-            password
+        const errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            this.users = dataSource.load(usersFilePath);
+            const {firstName, lastName, phone, email, password} = req.body;
+            const newUser = {
+                id: this.users.length ? this.users[this.users.length - 1].id + 1: 1,
+                firstName,
+                lastName,
+                phone,
+                email,
+                password
+            }
+            this.users.push(newUser);
+            dataSource.save(usersFilePath, this.users);
+            res.redirect("login");
+        }else{
+            res.render("register");
         }
-        this.users.push(newUser);
-        dataSource.save(usersFilePath, this.users);
-        res.redirect("login");
+
+
     },
     getLoginForm(req, res){
         res.render("login");
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const userValidationRules = require("../validators/userValidator");
 const userController = require("../controllers/userController");
 
 
 router.get("/register", userController.getRegisterForm);
-router.post("/register", userController.createUser);
+router.post("/register", userValidationRules, userController.createUser);
 router.get("/login", userController.getLoginForm);
 
 
