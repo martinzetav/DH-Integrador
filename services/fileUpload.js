1e27e2dc5004310f37127d10d8c82e6fba5c560f
Martin Zalazar <martinzetav@gmail.com>
2024-08-09 13:45:50 -0300

    feat: se configuro multer para su posterior uso

---
+++ services/fileUpload.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination(req, res, cb){
+        const folder = path.join(__dirname, "../public/images");
+        cb(null, folder);
+    },
+    filename(req, res, cb){
+        const fileName = path.parse(file.originalname).name;
+        const extension = path.extname(file.originalname);
+        const newFileName = `img-${fileName}-${Date.now()}${extension}`;
+        cb(null, newFileName);
+    }
+});
+
+const upload = multer({storage});
+
+module.exports = upload;
