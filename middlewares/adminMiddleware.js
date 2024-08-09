07638753343ca990e298683e78942a59e33ef17e
Martin Zalazar <martinzetav@gmail.com>
2024-08-09 13:11:22 -0300

    feat: se modifico adminMiddleware para redireccionar a usuarios sin acceso a editar

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,10 +1,17 @@
-function adminMiddleware(req, res, next){
+function adminMiddleware(req, res, next) {
     res.locals.isAdmin = false;
 
-    if(req.session.userLogged && req.session.userLogged.rol == "admin"){
+    if (req.session.userLogged && req.session.userLogged.rol === "admin") {
         res.locals.isAdmin = true;
+        next(); // Continúa si es administrador
+    } else {
+        // Solo redirigir si el usuario intenta acceder a la ruta de edición
+        if (req.path.includes('/edit/')) {
+            res.redirect(`/products/details/${req.params.id}`);
+        } else {
+            next(); // Permitir el acceso a otras rutas sin redirección
+        }
     }
-    next();
 }
 
 module.exports = adminMiddleware;
