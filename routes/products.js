01e296207e71807e769fcf5458ce0b7a3e0fecde
Martin Zalazar <martinzetav@gmail.com>
2024-08-09 19:59:28 -0300

    feat: se implemento la ruta para editar junto con el middleware upload

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../services/fileUpload");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 const productController = require("../controllers/productController");
 
 router.get("/:category", productController.getByCategory);
 router.get("/details/:id", adminMiddleware, productController.getById);
 router.get("/edit/:id", adminMiddleware, productController.getEditProductForm);
+router.put("/edit/:id", upload.single("image"), productController.updateProduct);
 
 module.exports = router;
